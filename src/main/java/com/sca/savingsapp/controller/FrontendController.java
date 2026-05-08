fd86586e952cd252eabbde8a23dedc96cc4ab9d0
classic792 <kobinagordon67@gmail.com>
2026-05-08 12:05:38 +0000

    Add frontend build files

---
+++ src/main/java/com/sca/savingsapp/controller/FrontendController.java
@@ -0,0 +1,16 @@
+package com.sca.savingsapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping(value = {
+            "/{path:^(?!api).*$}",
+            "/**/{path:^(?!api).*$}"
+    })
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
